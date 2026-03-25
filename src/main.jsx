d93ba97f18aee64b957512c865365352b5cacccc
ankkuuu <akshitbarthwal08@gmail.com>
2026-03-25 05:34:51 +0530

    package update

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './componants/Home.jsx'
 
 import { Provider } from 'react-redux'
