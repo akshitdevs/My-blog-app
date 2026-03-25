85a022404949969b649258c316895108be4e0362
ankkuuu <akshitbarthwal08@gmail.com>
2026-03-25 18:22:42 +0530

    RTE for mobile

--- src/componants/RTE.jsx
+++ src/componants/RTE.jsx
@@ -1,27 +1,40 @@
-// components/RTE.jsx
 import React from "react";
 import { Editor } from "@tinymce/tinymce-react";
 
 function RTE({ value, onChange }) {
   return (
     <Editor
-      apiKey="uey8v5o56lv6pjuuvb4b3xpcpfsj0xmzybx6yq116vfzsyw8" // works fine locally
+      apiKey="your-api-key"
       value={value}
       onEditorChange={onChange}
       init={{
         height: 400,
         menubar: false,
+
+        // Works on both devices
         skin: "oxide-dark",
         content_css: "dark",
-        plugins: [
-          "link",
-          "image",
-          "lists",
-          "code",
-          "table",
-        ],
+
+        plugins: ["link", "image", "lists", "code", "table"],
+
         toolbar:
-          "undo redo | bold italic | alignleft aligncenter alignright | bullist numlist | link image | code",
+          "undo redo | bold italic underline | alignleft aligncenter alignright | bullist numlist | link image | code",
+
+        // 👇 KEY PART (mobile support)
+        mobile: {
+          menubar: false,
+          toolbar:
+            "undo redo | bold italic | bullist numlist | link",
+        },
+
+        // 👇 improves touch behavior
+        toolbar_mode: "sliding",
+
+        // 👇 prevents weird focus bugs on mobile
+        browser_spellcheck: true,
+
+        // 👇 makes typing smoother on phones
+        contextmenu: false,
       }}
     />
   );
